7c6a2e418ba67fdae943d9151dc517c70d3ae299
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2014-12-11 22:54:05 +0100

    Laying the Foundation

---
+++ TekDays/test/unit/com/tekdays/TekEventSpec.groovy
@@ -0,0 +1,33 @@
+package com.tekdays
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(TekEvent)
+class TekEventSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test toString"() {
+        when: "a tekEvent has a name and city"
+            def tekEvent = new TekEvent(name: "Groovy One", city: "San Francisco", organizer: "John Doe")
+            
+        then: "the toString method will combine them"
+            tekEvent.toString() == "Groovy One, San Francisco"
+    }
+    
+    void "test description size"() {
+        when: "a tekEvent has a description"
+            def tekEvent = new TekEvent(description: "Some description")
+            
+        then: "the length must be less than or equal to 5000"
+            tekEvent.description.size() <= 5000
+    }
+}
